bc12a5df860251661fd704cc54c237f33940f4b1
Alessandro Benetton <aleben98@gmail.com>
2023-01-31 13:40:49 +0100

    [TO-11-watermark-detection-and-extraction] Separated watermarking tools

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -8,67 +8,32 @@
 
 // For more information on Content Scripts,
 // See https://developer.chrome.com/extensions/content_scripts
+import { WatermarkingTools } from "./watermarkingTools";
 
 // Custom CSS for highlighting
 const redHighlight: string = 'style="background-color: rgb(248 113 113);"';
 const greenHighlight: string = 'style="background-color: rgb(74 222 128);"';
 const blueHighlight: string = 'style="background-color: rgb(96 165 250);"';
 
-// Non Printable Character used as a delimiter
-//const NPC = '\u200b'  Zero Width Space
-const NPC = "§"; // <>;
-
-// Homoglyph character encoding
-const DecodeTableChar = {
-	"\u2010": "\u002d", // -
-	"\u037e": "\u003b", // ;
-	Ⅽ: "C", // C
-	Ⅾ: "D", // D
-	K: "K", // K
-	Ⅼ: "L", // L
-	Ⅿ: "M", // M
-	Ⅴ: "V", // V
-	Ⅹ: "X", // X
-	ⅽ: "c", // c
-	ⅾ: "d", // d
-	ⅰ: "i", // i
-	ј: "j", // j
-	ⅼ: "l", // l
-	ⅴ: "v", // v
-	ⅹ: "x" // x
-};
-
-// Homoglyph spaces encoding
-const TableSpace = [
-	"\u0020",
-	"\u2000",
-	"\u2004",
-	"\u2005",
-	"\u2008",
-	"\u2009",
-	"\u202f",
-	"\u205f"
-];
-
 const body = document.querySelector("body");
 
 if (body != null) {
-	if (body.innerHTML.includes(NPC)) {
+	if (body.innerHTML.includes(WatermarkingTools.NPC)) {
 		console.log("Non Printable Character found");
-		body.innerHTML = body.innerHTML.replaceAll(NPC, '<span ' + blueHighlight + '>' + NPC + "</span>");
+		body.innerHTML = body.innerHTML.replaceAll(WatermarkingTools.NPC, '<span ' + blueHighlight + '>' + WatermarkingTools.NPC + "</span>");
 	}
 
-	for (let i = 1; i < TableSpace.length; i++) {
-		if (body.innerHTML.includes(TableSpace[i])) {
+	for (let i = 1; i < WatermarkingTools.TableSpaces.length; i++) {
+		if (body.innerHTML.includes(WatermarkingTools.TableSpaces[i])) {
 			console.log("Homoglyph space found");
-			body.innerHTML = body.innerHTML.replaceAll(TableSpace[i], '<span ' + redHighlight + '>' + TableSpace[i] + "</span>");
+			body.innerHTML = body.innerHTML.replaceAll(WatermarkingTools.TableSpaces[i], '<span ' + redHighlight + '>' + WatermarkingTools.TableSpaces[i] + "</span>");
 		}
 	}
 
-	Object.keys(DecodeTableChar).forEach(function (key) {
-		if (body.innerHTML.includes(key)) {
+	WatermarkingTools.TableCharacters.homoglyph.forEach(function (char) {
+		if (body.innerHTML.includes(char)) {
 			console.log("Homoglyph char found");
-			body.innerHTML = body.innerHTML.replaceAll(key, '<span ' + greenHighlight + '>' + key + "</span>");
+			body.innerHTML = body.innerHTML.replaceAll(char, '<span ' + greenHighlight + '>' + char + "</span>");
 		}
 	});
 }
---
+++ src/watermarkingTools.ts
@@ -0,0 +1,157 @@
+export class WatermarkingTools {
+	public static readonly NPC: string = "\u00a7"; // <>;
+
+	private static readonly HomoglyphCharacters: string[] = [];
+
+	public static readonly TableCharacters: {
+		original: string[];
+		homoglyph: string[];
+	} = {
+		original: [
+			"\u002d",
+			"\u003b",
+			"\u0043",
+			"\u0044",
+			"\u004b",
+			"\u004c",
+			"\u004d",
+			"\u0056",
+			"\u0058",
+			"\u0063",
+			"\u0064",
+			"\u0069",
+			"\u006a",
+			"\u006c",
+			"\u0076",
+			"\u0078"
+		],
+		homoglyph: [
+			"\u2010",
+			"\u037e",
+			"\u216d",
+			"\u216e",
+			"\u212a",
+			"\u216c",
+			"\u216f",
+			"\u2164",
+			"\u2169",
+			"\u217d",
+			"\u217e",
+			"\u2170",
+			"\u0458",
+			"\u217c",
+			"\u2174",
+			"\u2179"
+		]
+	};
+
+	public static readonly TableSpaces: string[] = [
+		"\u0020",
+		"\u2000",
+		"\u2004",
+		"\u2005",
+		"\u2008",
+		"\u2009",
+		"\u202f",
+		"\u205f"
+	];
+
+	private static getSpaceCode(space : string): string {
+		// Find the element in the array
+		let index = this.TableSpaces.indexOf(space);
+
+		// If not found, return an empty string
+		if (index != -1) 
+			return "";
+		
+		// Convert the index to binary
+		let binary = index.toString(2);
+
+		return binary;
+	}
+
+	private static getSpaceFromCode(code : string): string {
+		// Convert the binary to an integer
+		let index = parseInt(code, 2);
+
+		// If not found, return an empty string
+		if (index != -1) 
+			return "";
+		
+		return this.TableSpaces[index];
+	}
+
+	private static getCharacterWithHomoglyph(character : string, value : string): string {
+		let charSet: string[] = value == "1"
+			? this.TableCharacters.homoglyph
+			: this.TableCharacters.original;
+
+		// Find the element in the array
+		let index = this.TableCharacters.original.indexOf(character);
+
+		// If not found, return an empty string
+		if (index != -1) 
+			return "";
+		
+		return charSet[index];
+	}
+
+	private static getCodeFromCharacter(character : string): string {
+		if (this.TableCharacters.original.indexOf(character) != -1) 
+			return "0";
+		if (this.TableCharacters.homoglyph.indexOf(character) != -1) 
+			return "1";
+		return "";
+	}
+
+	public static encodeText(text : string, binaryCode : string): string {
+		// Initializa output string with the NPC
+		var outText = this.NPC;
+
+		// Iterator index for binary code
+		var binIter = 0;
+		// Length of the binary code
+		const binLen = binaryCode.length;
+
+		// Iterate over the text
+		for (const c of text) {
+			if (c in this.TableCharacters.original.concat(this.TableCharacters.homoglyph)) {
+				outText = outText.concat(this.getCharacterWithHomoglyph(c, binaryCode.charAt(binIter)));
+				binIter += 1;
+			} else if (c in this.TableSpaces) {
+				// Get the binary code for the space (composed of 3 bits with eventual 0 padding)
+				var bitStr = "";
+				for (let i = binIter; i < binIter + 3; i++) {
+					if (i < binLen) {
+						bitStr = bitStr.concat(binaryCode[i]);
+					} else {
+						bitStr = bitStr.concat("0");
+					}
+				}
+				outText = outText.concat(this.getSpaceFromCode(bitStr));
+				binIter += 3;
+			} else {
+				outText = outText.concat(c);
+			}
+
+			// If the binary code is over, add the NPC and restart from 0
+			if (binIter >= binLen) {
+				outText = outText.concat(this.NPC);
+				binIter = 0;
+			}
+		}
+
+		return outText.concat(this.NPC);
+	}
+
+	// Decode a single paragraph
+	public static decodeParagraph(text : string): string {
+		return ""
+	}
+
+	// Split text based on NPC and call decodeParagraph on all paragraphs
+	public static decodeText(text : string): string {
+	
+		return ""
+	}
+}
