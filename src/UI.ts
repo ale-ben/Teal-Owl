951d634b342c6fa0f2958a8928b6744c346c4afc
Alessandro Benetton <aleben98@gmail.com>
2023-02-13 11:25:33 +0100

    [TO-4-continuous-watermarking-algorithm] Updated folder scheme

---
+++ src/UI.ts
@@ -0,0 +1,16 @@
+import {Utils} from "./utils";
+
+function onOpen() {
+	var ui = DocumentApp.getUi();
+	ui.createMenu("Watermarking Tools").addSubMenu(ui.createMenu("Apply watermark").addItem("Apply and export to PDF", "applyExportPDFButton")).addSeparator().addItem("Config", "openConfigButton").addToUi();
+}
+
+function applyExportPDFButton() {
+	Utils.applyExportPDF(DocumentApp.getActiveDocument());
+	DocumentApp.getUi().alert("Watermark applied correctly");
+}
+
+function openConfigButton() {
+	DocumentApp.getUi(). // Or DocumentApp or FormApp.
+	alert("This should open config popup");
+}
---
+++ src/utils.ts
@@ -0,0 +1,48 @@
+import {WatermarkingTools} from "./TOW/watermarkingTools";
+
+export module Utils {
+	function saveAsPDF(docFile : GoogleAppsScript.Drive.File, fileName : string) {
+		const docDoc = DocumentApp.openById(docFile.getId());
+		var pdfFile = DriveApp.createFile(docDoc.getAs("application/pdf"));
+		pdfFile.setName(fileName + ".pdf");
+		var fileParents = docFile.getParents();
+		var folder: GoogleAppsScript.Drive.Folder = fileParents.next();
+		while (fileParents.hasNext()) {
+			folder = fileParents.next();
+		}
+		pdfFile.moveTo(folder);
+	}
+
+	export function applyExportPDF(document : GoogleAppsScript.Document.Document) {
+		const docID = document.getId();
+		const fileName = docFile.getName();
+		const tmpFile = docFile.makeCopy("tmp_" + fileName); //TODO: Genero nuovo invece che fare copia potrebbe sistemare formattazione
+		const tmpDoc = DocumentApp.openById(tmpFile.getId());
+		const docBody = tmpDoc.getBody().editAsText();
+
+		const fakePayload = "0101010001100101011110000111010000100000011101110110000101110100011001010111001001101101011000010111001001101011011010010110111001100111001000000110100101110011001000000110000101110111011001010111001101101111011011010110010100100001";
+		const originalText = tmpDoc.getBody().getText();
+		console.log(originalText);
+		const watermarkedBody = WatermarkingTools.encodeText(originalText, fakePayload);
+		console.log(watermarkedBody);
+		docBody.setText(watermarkedBody);
+
+		//docBody.setText(tmpDoc.getBody().editAsText().getText());
+		docBody.setFontFamily("Arial");
+		docBody.setFontSize(11);
+		docBody.setTextAlignment(DocumentApp.TextAlignment.NORMAL);
+
+		var pars = tmpDoc.getBody().getParagraphs();
+		pars.forEach((par) => par.setAlignment(DocumentApp.HorizontalAlignment.LEFT));
+
+		tmpDoc.saveAndClose();
+
+		saveAsPDF(tmpFile, docFile.getName());
+		//tmpFile.setTrashed(true);
+	}
+}
+
+export function testApply() {
+	const doc = DocumentApp.openById("1JUhzMh3RAOVqns21JaIAWl-hD3Isz869YmbGb15pAH8");
+	Utils.applyExportPDF(doc);
+}
