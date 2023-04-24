4d41633f9689c0ed68bd6a38a614f224569310df
Alessandro Benetton <aleben98@gmail.com>
2023-04-24 18:29:54 +0200

    [feature/TO-6] Implemented config menu and parameters persistence

--- src/UI.ts
+++ src/UI.ts
@@ -12,17 +12,74 @@ function onOpen() {
 
 	mainDropdown.addSeparator();
 
-	mainDropdown.addItem("Config", "openConfigButton");
+	const configMenu = ui.createMenu("Config");
+	configMenu.addItem("Set config", "openConfigButton");
+	configMenu.addItem("View config", "viewConfigButton");
+	configMenu.addItem("Validate config", "validateConfigButton");
+	configMenu.addItem("Reset config", "resetConfigButton");
+	mainDropdown.addSubMenu(configMenu);
 
 	mainDropdown.addToUi();
 }
 
 function applyExportPDFButton() {
-	const outFile = Utils.applyExport(DocumentApp.getActiveDocument());
-	DocumentApp.getUi().alert("Watermark applied correctly. Output file: " + outFile.getName() + ". Location: " + outFile.getUrl());
+	const ui = DocumentApp.getUi();
+	if (checkConfig()) {
+		const outFile = Utils.applyExport(DocumentApp.getActiveDocument());
+		ui.alert("Watermark applied correctly. Output file: " + outFile.getName() + ". Location: " + outFile.getUrl());
+	} else {
+		ui.alert("Please run config in the watermarking menu.");
+	}
+}
+
+function checkConfig(): boolean {
+	const userProperties = PropertiesService.getUserProperties();
+	return userProperties.getProperty("userID") != null && userProperties.getProperty("userID") != "";
+}
+
+function validateConfigButton() {
+	const ui = DocumentApp.getUi();
+	if (checkConfig()) {
+		ui.alert("Config is valid.");
+	} else {
+		ui.alert("Config is invalid.");
+	}
+}
+
+function viewConfigButton() {
+	const userProperties = PropertiesService.getUserProperties();
+	const ui = DocumentApp.getUi();
+
+	const userID = userProperties.getProperty("userID");
+
+	ui.alert("User ID: " + (
+		userID
+			? userID
+			: ""
+	));
+}
+
+function resetConfigButton() {
+	const userProperties = PropertiesService.getUserProperties();
+	const ui = DocumentApp.getUi();
+
+	userProperties.deleteAllProperties();
+	ui.alert("Config reset.");
 }
 
 function openConfigButton() {
-	DocumentApp.getUi(). // Or DocumentApp or FormApp.
-	alert("This should open config popup");
+	const userProperties = PropertiesService.getUserProperties();
+	const ui = DocumentApp.getUi();
+
+	const userID = userProperties.getProperty("userID");
+
+	var userIDPrompt = ui.prompt("Please enter your user ID", "Current user ID is: " + (
+		userID
+			? userID
+			: ""
+	), ui.ButtonSet.OK_CANCEL);
+	if (userIDPrompt.getSelectedButton() == ui.Button.OK && userIDPrompt.getResponseText() != "") {
+		userProperties.setProperty("userID", userIDPrompt.getResponseText());
+		ui.alert("User ID updated, new user ID: " + userIDPrompt.getResponseText());
+	}
 }
