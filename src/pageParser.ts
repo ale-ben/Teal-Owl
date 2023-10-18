cc534c546a46cdbe729b16b99442caa9cbea81f6
Alessandro Benetton <aleben98@gmail.com>
2023-10-18 11:42:27 +0200

    [feature/TO-11] Working parser

--- src/pageParser.ts
+++ src/pageParser.ts
@@ -1,3 +1,9 @@
+// interface WMParagraph {
+// 	begin: Element;
+// 	status: number;
+// 	crossParagraph: WMParagraph | undefined;
+// }
+
 /*
  * Status codes:
  * 0 - Empty
@@ -38,7 +44,7 @@ function setStatus(meta: Element, status: number): number {
  * Entrypoint for the page parser, called by onClicked event in background.ts
  */
 export function toggleReader() {
-	console.log('parseContent');
+	console.log('toggleReader');
 	let status: number;
 
 	// Check if meta tag exists
@@ -66,11 +72,61 @@ export function toggleReader() {
 		default:
 			// Page has not been parsed yet
 			parsePage();
-			setStatus(meta, 1);
+			//setStatus(meta, 1);
 			break;
 	}
 }
 
 function parsePage() {
-	//const body = document.body;
+	console.log('parsePage');
+	const body = document.body.innerHTML;
+
+	// This will contain the new body with watermarking tags
+	let newBody = '';
+
+	// Index of last character added to newBody
+	let lastIndex = 0;
+
+	// Boolean to control wether to add open or close tag
+	let newPar: boolean = true;
+
+	// Index of watermarking tag
+	let wmIndex = 0;
+
+	// Non Printable Character used to separate paragraphs
+	const NPC = '§';
+
+	for (let index = 0; index < body.length; index += 1) {
+		// If you find a NPC, add a watermarking tag
+		if (body[index] === '§') {
+			// Save everything preceding NPC in newBody
+			if (index > 0) {
+				newBody += body.substring(lastIndex, index);
+			}
+
+			// Add watermarking tag (newPar determines if open or close tag)
+			if (newPar) {
+				// Add open tag, increment wmIndex and set newPar to false
+				newBody +=
+					'<mark class="watermark-marktag" id="watermark-' +
+					wmIndex +
+					'">' +
+					NPC;
+				wmIndex += 1;
+				newPar = false;
+			} else {
+				// Add close tag and set newPar to true
+				newBody += NPC + '</mark>';
+				newPar = true;
+			}
+			// Update lastIndex to current index
+			lastIndex = index + 1;
+		}
+	}
+
+	// Add everything after last NPC to newBody
+	newBody += body.substring(lastIndex, body.length);
+
+	// Replace body with newBody
+	document.body.innerHTML = newBody;
 }
