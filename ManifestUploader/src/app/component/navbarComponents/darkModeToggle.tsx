8444e266d5fe68982f876ad497c6770684cd80c0
Alessandro Benetton <aleben98@gmail.com>
2023-11-23 10:56:54 +0100

    [feature/TO-43] Custom dark mode toggle

--- ManifestUploader/src/app/component/navbarComponents/darkModeToggle.tsx
+++ ManifestUploader/src/app/component/navbarComponents/darkModeToggle.tsx
@@ -1,46 +1,18 @@
 'use client';
-import { VisuallyHidden } from '@nextui-org/react';
-import { useSwitch, SwitchProps } from '@nextui-org/switch';
-import { useEffect } from 'react';
 import { MdLightMode, MdDarkMode } from 'react-icons/md';
-import {useTheme} from "next-themes";
+import { useTheme } from 'next-themes';
+import { Button } from '@nextui-org/button';
 
-const DarkModeToggle = (props: SwitchProps) => {
-	const {
-		Component,
-		slots,
-		isSelected,
-		getBaseProps,
-		getInputProps,
-		getWrapperProps
-	} = useSwitch(props);
-
+const DarkModeToggle = () => {
 	const { theme, setTheme } = useTheme();
 
-	useEffect(() => {
-		setTheme(isSelected ? 'dark' : 'light')
-	}, [isSelected]);
-
 	return (
-		<div className="flex flex-col gap-2">
-			<Component {...getBaseProps()}>
-				<VisuallyHidden>
-					<input {...getInputProps()} />
-				</VisuallyHidden>
-				<div
-					{...getWrapperProps()}
-					className={slots.wrapper({
-						class: [
-							'h-8 w-8',
-							'flex items-center justify-center',
-							'rounded-lg bg-default-100 hover:bg-default-200'
-						]
-					})}
-				>
-					{isSelected ? <MdLightMode /> : <MdDarkMode />}
-				</div>
-			</Component>
-		</div>
+		<Button
+			isIconOnly
+			onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+		>
+			{theme === 'light' ? <MdDarkMode className="text-xl"/> : <MdLightMode className="text-xl"/>}
+		</Button>
 	);
 };
 
