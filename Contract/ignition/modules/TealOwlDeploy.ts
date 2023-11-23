abf1b8784aafbbdb355f9791b91bc0d034a57ee2
Alessandro Benetton <aleben98@gmail.com>
2023-11-23 09:37:43 +0100

    [feature/TO-44] booh

--- Contract/ignition/modules/TealOwlDeploy.ts
+++ Contract/ignition/modules/TealOwlDeploy.ts
@@ -3,7 +3,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const admin = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 const minter = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
 
-export default buildModule("TealOwlDeploy1", (m) => {
+export default buildModule("TealOwlDeploy", (m) => {
   const TOContract = m.contract("TealOwl", [admin, minter]);
 
   return { TOContract };
