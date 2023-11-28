3cb7bf2d08df685602c07671fa453b5761441952
Alessandro Benetton <aleben98@gmail.com>
2023-11-28 10:54:49 +0100

    [feature/TO-43] Add favicon

--- ManifestUploader/src/app/layout.tsx
+++ ManifestUploader/src/app/layout.tsx
@@ -15,6 +15,27 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" className="dark" suppressHydrationWarning>
+			<head>
+				<link rel="icon" href="static/icons/favicon.ico" sizes="48x48" />
+				<link
+					rel="icon"
+					href="static/icons/icon32x32.png"
+					type="image/png"
+					sizes="32x32"
+				/>
+				<link
+					rel="icon"
+					href="static/icons/icon16x16.png"
+					type="image/png"
+					sizes="16x16"
+				/>
+				<link
+					rel="apple-touch-icon"
+					href="static/icons/apple-icon.png"
+					type="image/png"
+					sizes="180x180"
+				/>
+			</head>
 			<body>
 				<Providers>
 					<TONavbar />
