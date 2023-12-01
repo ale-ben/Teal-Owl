4759f2fa8be4e5993f0225d39541117d495d0ca9
Alessandro Benetton <aleben98@gmail.com>
2023-12-01 16:42:37 +0100

    [feature/TO-45] Invalidate leaked keys and move them to JSON file

--- Contract/hardhat.config.ts
+++ Contract/hardhat.config.ts
@@ -1,10 +1,11 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ignition";
+import {InfuriaAPIKEY, SepoliaPrivKey, EthersanAPIKey} from "./credentials.json"
 
 // Go to https://infura.io, sign up, create a new API key
 // in its dashboard, and replace "KEY" with it
-const INFURA_API_KEY = "f59b7e6611db42eca5466757b3ed27f7";
+// const INFURA_API_KEY = "";
 
 // Replace this private key with your Sepolia account private key
 // To export your private key from Coinbase Wallet, go to
@@ -12,18 +13,18 @@ const INFURA_API_KEY = "f59b7e6611db42eca5466757b3ed27f7";
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const SEPOLIA_PRIVATE_KEY = "2714478a37578c1246c578ee38ca849214c480cecf6fb882b2dd64120ced4aa0";
+// const SEPOLIA_PRIVATE_KEY = "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.23",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY]
+      url: `https://sepolia.infura.io/v3/${InfuriaAPIKEY}`,
+      accounts: [SepoliaPrivKey]
     }
   },
   etherscan: {
-    apiKey: "33ZNH125QE12CY2QETFH9IVM1ZGANJ47QU",
+    apiKey: EthersanAPIKey,
   }
 };
 
