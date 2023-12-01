1684197e5220d1a91e1d7f84e8cf0837fa64f442
Alessandro Benetton <aleben98@gmail.com>
2023-12-01 17:56:33 +0100

    [feature/TO-44] Refactor uploader component

--- ManifestUploader/src/app/page.tsx
+++ ManifestUploader/src/app/page.tsx
@@ -1,49 +1,10 @@
-'use client';
-
-import { getName } from '@/utils/contractUtils';
-import { Button } from '@nextui-org/button';
-import { useEffect, useState } from 'react';
-import { useAccount } from 'wagmi';
+import { redirect } from "next/navigation";
 
 export default function Home() {
-	const { isConnected } = useAccount();
-	const [name, setName] = useState('');
-	const [pageReady, setPageReady] = useState(false);
-
-	useEffect(() => {
-		setPageReady(true);
-	}, []);
-
+	redirect('/upload');
 	return (
 		<div className="m-5">
-			<p className="pb-3 text-center text-3xl">Upload a new manifest</p>
-			{
-				//<UploadComponent />
-			}
-			<div>
-				<p className="text-3xl">Contract name: {name}</p>
-			</div>
-
-			<Button
-				isDisabled={!pageReady || !isConnected}
-				onPress={() => {
-					console.log('pressed');
-					getName()
-						.then((name) => {
-							if (name) {
-								console.log('name', name);
-								setName(name);
-							} else {
-								console.log('no name');
-							}
-						})
-						.catch((err) => {
-							console.error(err);
-						});
-				}}
-			>
-				getName
-			</Button>
+			<p className="pb-3 text-center text-3xl">Home page</p>
 		</div>
 	);
 }
