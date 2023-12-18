6e4a45c16db0d3b51362aae64db4ec33b9d4da69
Alessandro Benetton <aleben98@gmail.com>
2023-12-18 15:35:39 +0100

    [develop] revert to basic next config

--- apps/backend/next.config.js
+++ apps/backend/next.config.js
@@ -1,19 +1,10 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	webpack: (config) => {
 		config.externals.push('pino-pretty', 'lokijs', 'encoding');
 		return config;
 	},
-	output: 'standalone',
-	experimental: {
-		// this includes files from the monorepo base two directories up
-		outputFileTracingRoot: path.join(__dirname, '../../'),
-		turbotrace: {
-			logLevel: 'warning'
-		}
-	}
+	output: 'standalone'
 };
 
 module.exports = nextConfig;
