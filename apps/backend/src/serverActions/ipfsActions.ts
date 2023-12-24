6148d7a8b21b9677cc68bb6ee9001889822a21ad
Alessandro Benetton <aleben98@gmail.com>
2023-12-24 17:32:02 +0100

    [feature/TO-18] Linting backend

--- apps/backend/src/serverActions/ipfsActions.ts
+++ apps/backend/src/serverActions/ipfsActions.ts
@@ -1,17 +1,23 @@
 'use server';
 
-import {IPFSObject, uploadManifestToIPFS} from '@teal-owl/ipfs-utils';
+import { IPFSObject, uploadManifestToIPFS } from '@teal-owl/ipfs-utils';
 
 const apiKey = process.env.THIRDWEB_API_KEY;
 
 if (apiKey === undefined) {
-	console.warn('No THIRDWEB_API_KEY environment variable found. Skipping IPFS setup.');
+	console.warn(
+		'No THIRDWEB_API_KEY environment variable found. Skipping IPFS setup.'
+	);
 }
 
-export async function uploadToIPFS(obj: IPFSObject): Promise<IPFSObject | undefined> {
+export async function uploadToIPFS(
+	obj: IPFSObject
+): Promise<IPFSObject | undefined> {
 	if (apiKey === undefined) {
-		console.warn('No THIRDWEB_API_KEY environment variable found. Skipping IPFS upload.');
-		return undefined
-	};
+		console.warn(
+			'No THIRDWEB_API_KEY environment variable found. Skipping IPFS upload.'
+		);
+		return undefined;
+	}
 	return uploadManifestToIPFS(obj, apiKey);
 }
